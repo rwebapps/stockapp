67c52005ad1b0b0da89fd756b7d82963f25c9f54
Jeroen Ooms <jeroenooms@gmail.com>
2013-12-03 15:52:20 -0800

    fix for opencpu 1.2

---
+++ R/listbyindustry.R
@@ -0,0 +1,15 @@
+listbyindustry <- function(){
+  newtable <- with(stocktable, data.frame(id=paste0("symbol_", Ticker), text=paste(Ticker, "-", Company), leaf=TRUE, Industry=Industry, Sector=Sector));
+  mydata <- splitIntoTree(newtable, "Sector");
+  mydata$children <- lapply(mydata$children, splitIntoTree, f="Industry", out=c("id", "text", "leaf"));
+  return(list("text"=".", children=mydata))
+}
+
+splitIntoTree <- function(x, f, out){
+  stopifnot(!missing(f))
+  mydata <- split(x, x[f], drop=TRUE);
+  if(!missing(out)){
+    mydata <- lapply(mydata, "[", out)
+  }
+  structure(list(names(mydata), unname(mydata)), class="data.frame", names=c("text", "children"), row.names=as.character(seq_along(mydata)));
+}
