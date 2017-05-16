a22fe0bddeddebf1a85523715afa54602cdc8bce
Jeroen Ooms <jeroenooms@gmail.com>
2017-05-16 18:33:48 +0200

    Add update function to remove dead stocks

---
+++ R/update.R
@@ -0,0 +1,24 @@
+update_stocktable <- function(){
+  ticker_ok <- check_tickers(stocktable$Ticker)
+  stocktable <- stocktable[ticker_ok,]
+  save(stocktable, file = "data/stocktable.rda")
+}
+
+check_tickers <- function(tickers){
+   vapply(tickers, function(symbol){
+    out <- symbol_exists(symbol)
+    cat(sprintf("%s: %s\n", symbol, ifelse(out, "OK", "DEAD")))
+    Sys.sleep(1); gc()
+    return(out)
+  }, logical(1))
+}
+
+symbol_exists <- function(symbol){
+  for(i in 1:3){
+    try({
+      yahoodata(symbol, "2013-01-01", as.character(Sys.Date()))
+      return(TRUE)
+    }, silent = TRUE)
+  }
+  return(FALSE)
+}
